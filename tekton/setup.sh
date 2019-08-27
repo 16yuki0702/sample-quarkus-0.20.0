069732263b10a3ddda531018985f33101ce53477
16yuki0702 <hs19870702@gmail.com>
2019-08-27 11:26:19 +0900

    Modify script

--- tekton/setup.sh
+++ tekton/setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-oc new-project pipeline-test
+oc new-project $1-pipeline
+oc project $1-pipeline
 oc create serviceaccount pipeline
 oc adm policy add-scc-to-user privileged -z pipeline
 oc adm policy add-role-to-user edit -z pipeline
