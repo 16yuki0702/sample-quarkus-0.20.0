8500faba0a50c7346c155ed8792d7d633c33ba05
16yuki0702 <hs19870702@gmail.com>
2019-08-27 11:10:25 +0900

    Modify start pipeline.sh

--- tekton/start-pipeline.sh
+++ tekton/start-pipeline.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
 oc apply -f application.yaml
-oc apply -f pipeline-task.yaml
+oc apply -f tasks.yaml
 oc apply -f pipeline-resources.yaml
-oc apply -f pipeline-task-run.yaml
-
-tkn pipeline start sample-pipeline \
-  -r app-source=pipeline-source \
-  -r app-image=pipeline-image \
-  -s pipeline
+oc apply -f pipeline.yaml
+oc apply -f pipeline-run.yaml
