cf092dd1ee16562ecba73fc70898a0b1bf0295c9
16yuki0702 <hs19870702@gmail.com>
2019-09-02 13:38:47 +0900

    Modify setup and add task-runs

--- tekton/setup.sh
+++ tekton/setup.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-oc new-project pipeline-test
-oc project pipeline-test
-oc create serviceaccount pipeline
-oc adm policy add-scc-to-user privileged -z pipeline
-oc adm policy add-role-to-user edit -z pipeline
+for i in {1..40}
+do
+  PROJECT=`printf "dev%02d-pipeline" "${i}"`
+  USERNAME=`printf "dev%02d" "${i}"`
+  oc new-project $PROJECT --as=$USERNAME \
+    --as-group=system:authenticated --as-group=system:authenticated:oauth
+  oc project $PROJECT
+  oc create serviceaccount pipeline
+  oc adm policy add-scc-to-user privileged -z pipeline
+  oc adm policy add-role-to-user edit -z pipeline
+  sleep 3
+done
